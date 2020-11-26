b50746c5d38d06c6d70858d1bd8e4b88908629d3
Eric <eric.amilhat@epitech.eu>
2020-11-26 11:59:30 +0100

    added Data

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+
+
 const express = require('express')
+
+const data = require("./data.json")
+
 const app = express()
 const port = process.env.PORT || 3000
 
+function formatDate(date) {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const date = req.query.date
+  if (!date || date === "") {
+      res.send(data[formatDate(new Date())])
+      return
+  }
+
+  if (data[date]) {
+    res.send(data[date])
+    return
+  }
+
+  res.sendStatus(404)
 })
 
 app.listen(port, () => {
